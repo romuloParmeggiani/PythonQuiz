c8f3753e5060e15447343a91a2bfc66b2933dbd2
romuloParmeggiani <romulo.parmeggiani@gmail.com>
2023-08-09 11:05:54 -0300

    Creation of Question classes.

---
+++ QuizzAppFoundation/quiz.py
@@ -0,0 +1,19 @@
+class Question:
+    def __init__(self):
+        # TODO: define the Question fields.
+        pass
+
+class QuestionTrueOrFalse(Question):
+    def __init__(self):
+        super().__init__()
+    
+    # TODO: define True or False ask method.
+    def ask(self):
+        pass
+class QuestionMultiChoice(Question):
+    def __init__(self):
+        super().__init__()
+    
+    # TODO: define Multi Choice ask method.
+    def ask(self):
+        pass
